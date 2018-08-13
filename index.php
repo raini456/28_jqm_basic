b3e3d7c6f86ae17e2d124df5367eeb40583c4236
pr08 <pr08@figd-1711708.figdtk.de>
2018-08-13 15:20:19 +0200

    1st Commmit 13.08.

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>jQuery Mobile page</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="assets/css/themes/mycolors.css" />
+        <link rel="stylesheet" href="assets/css/themes/jquery.mobile.icons.min.css" />
+        <!--<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile.structure-1.4.5.min.css" /> 
+        <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script> 
+        <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script> -->
+        <link rel="stylesheet" href="assets/css/jquery.mobile.structure-1.4.5.min.css" /> 
+        <script src="assets/js/jquery-1.11.1.min.js"></script> 
+        <script src="assets/js/jquery.mobile-1.4.5.min.js"></script>
+
+
+    </head>
+    <body>
+
+        <div data-role="page">
+            <div data-role="header">
+                <h1>Looft</h1>
+            </div>
+            <div data-role="content">
+
+            </div>
+            <div data-role="footer">
+                <p>&copy; 2018</p>
+            </div>
+        </div>
+
+
+    </body>
+</html>
